26716902d354faa031f79f0992db7788874f6b71
beyondnaxos <86872427+beyondnaxos@users.noreply.github.com>
2022-01-06 17:20:16 +0100

    remise en forme modele

--- back/models/user.js
+++ back/models/user.js
@@ -1,12 +1,28 @@
-module.exports = (sequelize, Sequelize) => {
-    const User = sequelize.define('User', {
-      email: Sequelize.STRING,
-      username: Sequelize.STRING,
-      password: Sequelize.STRING
-    }, {});
-    User.associate = function(models) {
-      // les associations peuvent être définies ici
-    };
+// module.exports = (sequelize, Sequelize) => {
+//     const User = sequelize.define('user', {
+//       email: Sequelize.STRING,
+//       username: Sequelize.STRING,
+//       password: Sequelize.STRING
+//     });
+//     // User.associate = function(models) {
+//     //   // les associations peuvent être définies ici
+//     // };
     
-    return User;
-  };
+//     return User;
+//   };
+
+  module.exports = (sequelize, Sequelize) => {
+    const User = sequelize.define("user", {
+      email: {
+        type: Sequelize.STRING
+      },
+      username: {
+        type: Sequelize.STRING
+      },
+      password: {
+        type: Sequelize.STRING
+      }
+    })
+  
+    return User
+  }
--- back/server.js
+++ back/server.js
@@ -1,66 +1,65 @@
-const express = require('express')
-const cors = require('cors')
-require('dotenv').config()
+const express = require("express");
 
-const app = express()
+const cors = require("cors");
+  
+require("dotenv").config();
+
+const app = express();
 
 const corsOptions = {
-    origin: 'http://localhost:3000'
-}
+  origin: "http://localhost:3000",
+};
 
-app.use(cors(corsOptions))
+app.use(cors(corsOptions));
 
 // -------------------------------------------------
 // test helmet
-const helmet = require('helmet');
+const helmet = require("helmet");
 app.use(helmet());
 // -------------------------------------------------
 // -------------------------------------------------
-// test rate limit etc 
+// test rate limit etc
 const rateLimit = require("express-rate-limit");
 const limiter = rateLimit({
-    windowMs: 0.1 * 60 * 1000,
-    max: 100 
-  })
-app.use(limiter)
+  windowMs: 0.1 * 60 * 1000,
+  max: 100,
+});
+app.use(limiter);
 // -------------------------------------------------
 // -------------------------------------------------
-// test user routes  
-const userRoutes = require('./routes/user') 
+// test user routes
+const userRoutes = require("./routes/user");
 
 // -------------------------------------------------
-
-
 
 // prise en charge du JSON
-app.use(express.json())
+app.use(express.json());
 
 // parse requests of content-type - application/x-www-form-urlencoded
-app.use(express.urlencoded({ extend: true }))
+app.use(express.urlencoded({ extend: true }));
 
 // simple route
 app.get("/", (req, res) => {
-    res.json({ message: "Mon appli est fonctionelle" })
-})
+  res.json({ message: "Mon appli est fonctionelle" });
+});
 
 // set port, listen for requests
-const PORT = process.env.PORT || 8080
-require('./routes/tutorial.routes.js')(app)
+const PORT = process.env.PORT || 8080;
+require("./routes/tutorial.routes.js")(app);
 app.listen(PORT, () => {
-    console.log(` Le serveur est en écoute sur le port ${PORT}.`)
-})
+  console.log(` Le serveur est en écoute sur le port ${PORT}.`);
+});
 
-
-const db = require('./models')
+const db = require("./models");
 
 const controller = require("./controllers/tutorial.controller");
 
 const run = async () => {
-    const tut1 = await controller.createTutorial({
-        title: "Tut#1",
-        description: "Tut#1 Description",
-      });
-      /*
+  const tut1 = await controller.createTutorial({
+    title: "Tut#1",
+    description: "Tut#1 Description",
+  });
+  /*
       >> Created tutorial: {
           "id": 1,
           "title": "Tut#1",
@@ -69,12 +68,12 @@ const run = async () => {
           "createdAt": "2020-04-14T09:49:14.021Z" 
       }
       */
-      
-      const tut2 = await controller.createTutorial({
-        title: "Tut#2",
-        description: "Tut#2 Description",
-      });
-      /*
+
+  const tut2 = await controller.createTutorial({
+    title: "Tut#2",
+    description: "Tut#2 Description",
+  });
+  /*
       >> Created tutorial: {
           "id": 2,
           "title": "Tut#2",
@@ -84,11 +83,11 @@ const run = async () => {
       }
       */
 
-      const comment1 = await controller.createComment(tut1.id, {
-        name: "bezkoder",
-        text: "Good job!",
-      });
-      /*
+  const comment1 = await controller.createComment(tut1.id, {
+    name: "bezkoder",
+    text: "Good job!",
+  });
+  /*
       >> Created comment: {
           "id": 1,
           "name": "bezkoder",
@@ -98,12 +97,12 @@ const run = async () => {
           "createdAt": "2020-04-14T09:49:14.071Z"
       }
       */
-      
-      await controller.createComment(tut1.id, {
-        name: "zkoder",
-        text: "One of the best tuts!",
-      });
-      /*
+
+  await controller.createComment(tut1.id, {
+    name: "zkoder",
+    text: "One of the best tuts!",
+  });
+  /*
       >> Created comment: {
           "id": 2,
           "name": "zkoder",
@@ -113,12 +112,12 @@ const run = async () => {
           "createdAt": "2020-04-14T09:49:14.081Z"
       }
       */
-      
-      const comment2 = await controller.createComment(tut2.id, {
-        name: "aKoder",
-        text: "Hi, thank you!",
-      });
-      /*
+
+  const comment2 = await controller.createComment(tut2.id, {
+    name: "aKoder",
+    text: "Hi, thank you!",
+  });
+  /*
       >> Created comment: {
           "id": 3,
           "name": "aKoder",
@@ -128,12 +127,12 @@ const run = async () => {
           "createdAt": "2020-04-14T09:49:14.855Z"
       }
       */
-      
-      await controller.createComment(tut2.id, {
-        name: "anotherKoder",
-        text: "Awesome tut!",
-      });
-      /*
+
+  await controller.createComment(tut2.id, {
+    name: "anotherKoder",
+    text: "Awesome tut!",
+  });
+  /*
       >> Created comment: {
           "id": 4,
           "name": "anotherKoder",
@@ -144,12 +143,12 @@ const run = async () => {
       }
       */
 
-      const tut1Data = await controller.findTutorialById(tut1.id);
-console.log(
-  ">> Tutorial id=" + tut1Data.id,
-  JSON.stringify(tut1Data, null, 2)
-);
-/*
+  const tut1Data = await controller.findTutorialById(tut1.id);
+  console.log(
+    ">> Tutorial id=" + tut1Data.id,
+    JSON.stringify(tut1Data, null, 2)
+  );
+  /*
 >> Tutorial id=1 {
   "id": 1,
   "title": "Tut#1",
@@ -177,12 +176,12 @@ console.log(
 }
 */
 
-const tut2Data = await controller.findTutorialById(tut2.id);
-console.log(
-  ">> Tutorial id=" + tut2Data.id,
-  JSON.stringify(tut2Data, null, 2)
-);
-/*
+  const tut2Data = await controller.findTutorialById(tut2.id);
+  console.log(
+    ">> Tutorial id=" + tut2Data.id,
+    JSON.stringify(tut2Data, null, 2)
+  );
+  /*
 >> Tutorial id=2 {
   "id": 2,
   "title": "Tut#2",
@@ -210,12 +209,12 @@ console.log(
 }
 */
 
-const comment1Data = await controller.findCommentById(comment1.id);
-console.log(
-  ">> Comment id=" + comment1.id,
-  JSON.stringify(comment1Data, null, 2)
-);
-/*
+  const comment1Data = await controller.findCommentById(comment1.id);
+  console.log(
+    ">> Comment id=" + comment1.id,
+    JSON.stringify(comment1Data, null, 2)
+  );
+  /*
 >> Comment id=1 {
   "id": 1,
   "name": "bezkoder",
@@ -232,12 +231,12 @@ console.log(
   }
 }
 */
-const comment2Data = await controller.findCommentById(comment2.id);
-console.log(
-  ">> Comment id=" + comment2.id,
-  JSON.stringify(comment2Data, null, 2)
-);
-/*
+  const comment2Data = await controller.findCommentById(comment2.id);
+  console.log(
+    ">> Comment id=" + comment2.id,
+    JSON.stringify(comment2Data, null, 2)
+  );
+  /*
 >> Comment id=3 {
   "id": 3,
   "name": "aKoder",
@@ -255,9 +254,9 @@ console.log(
 }
 */
 
-const tutorials = await controller.findAll();
-console.log(">> All tutorials", JSON.stringify(tutorials, null, 2));
-/*
+  const tutorials = await controller.findAll();
+  console.log(">> All tutorials", JSON.stringify(tutorials, null, 2));
+  /*
 >> All tutorials [
   {
     "id": 1,
@@ -312,11 +311,11 @@ console.log(">> All tutorials", JSON.stringify(tutorials, null, 2));
 ]
 */
 };
-  
-// db.sequelize.sync();
-  db.sequelize.sync({ force: true }).then(() => {
-    console.log("Drop and re-sync db.");
-    run();
-  });
 
-  app.use('/api/auth', userRoutes) 
+// db.sequelize.sync();
+db.sequelize.sync({ force: true }).then(() => {
+  console.log("Drop and re-sync db.");
+  run();
+});
+
+app.use("/api/auth", userRoutes);
